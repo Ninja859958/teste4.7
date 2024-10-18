6b380789186b60b293ce72ab039b99d2bc321053
ninja <alemaomichael@hotmail.com>
2024-10-18 14:06:37 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+=======
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+>>>>>>> bd764ba (update h1)
+}
